09e1beb40b6b59b81719667a35bf9089b4f3f9bf
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-01 19:17:59 +0200

    dropdown closes on click outside

--- app/components/NavAppearanceDropDown.tsx
+++ app/components/NavAppearanceDropDown.tsx
@@ -35,6 +35,27 @@ function AppearanceDropDownItem({
 }
 
 function AppearanceDropDown() {
+  // Close the appearance dropdown if click outside
+  window.addEventListener("click", (event) => {
+    const appearanceDropdown = document.getElementById("appearance-drop-down");
+    const navbuttonAppearance = document.getElementById("navbutton-appearance");
+    if (!appearanceDropdown || !navbuttonAppearance || !event.target) {
+      return;
+    }
+    if (!appearanceDropdown.classList.contains("hidden")) {
+      if (
+        event.target != appearanceDropdown &&
+        event.target != navbuttonAppearance &&
+        (event.target as HTMLElement).parentNode != navbuttonAppearance
+      ) {
+        // hide
+        appearanceDropdown.classList.add("hidden");
+        navbuttonAppearance.classList.remove("drop-down-visible");
+        navbuttonAppearance.classList.add("drop-down-hidden");
+      }
+    }
+  });
+
   return (
     <div className="hidden" id="appearance-drop-down">
       <div className="card-border"></div>
--- app/components/NavHome.tsx
+++ app/components/NavHome.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 function NavHome() {
   return (
     <div
