032a7197825b6d20eef7c2f42cd01ca120e24d9a
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-13 16:08:51 +0200

    added actordentify card

--- app/components/ActordentifyCard.tsx
+++ app/components/ActordentifyCard.tsx
@@ -1,4 +1,6 @@
 import Card from "./Card";
+import NavLink from "./NavLink";
+import "./ActordentifyCard.css";
 
 type Props = {
   cardData: { [key: string]: any };
@@ -7,7 +9,33 @@ type Props = {
 function ActordentifyCard({ cardData }: Props) {
   return (
     <Card id="actordentify-card" individualEffect={false}>
-      <h3>{cardData.title}</h3>
+      <div className="card-images">
+        <video
+          src="videos/actordentify-hero.mp4"
+          // autoPlay temporary until I write autoplay on scroll
+          autoPlay
+          playsInline
+          muted
+          loop
+          id="actordentify-card-video-bkg"
+        />
+        <video
+          src="videos/actordentify-hero.mp4"
+          // autoPlay temporary until I write autoplay on scroll
+          autoPlay
+          playsInline
+          muted
+          loop
+          id="actordentify-card-video"
+        />
+        <div className="frosted-pane">
+          <h3>{cardData.title}</h3>
+          <p className="paragraph-small">{cardData.subtitle}</p>
+          <NavLink href={cardData.link}>
+            <p className="card-link">Learn more</p>
+          </NavLink>
+        </div>
+      </div>
     </Card>
   );
 }
