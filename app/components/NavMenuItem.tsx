92993d22b7eef8072cd8c0a25defd7d3e20de188
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-21 21:52:42 +0200

    navmenu can now scroll onclick

--- app/components/NavMenuItem.tsx
+++ app/components/NavMenuItem.tsx
@@ -6,11 +6,34 @@ type Props = {
 function NavMenuItem({ name, isDefault = false }: Props) {
   const nameLowerDashed = name.toLowerCase().replace(" ", "-");
 
+  function onClick(nameLowerDashed: string) {
+    const sectionName =
+      nameLowerDashed === "about-me" ? "hero" : nameLowerDashed;
+    const navmenu = document.getElementById("navmenu-mobile");
+    if (!navmenu) {
+      return;
+    }
+    if (navmenu.classList.contains("hidden")) {
+      // unhideNavmenu();
+    } else {
+      if (!document.getElementById(sectionName)) {
+        return;
+      }
+      const section = document.getElementById(sectionName) as HTMLElement;
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+      });
+      // hideNavmenu();
+    }
+  }
+
   return (
     <div
       className={`navmenu-item ${isDefault ? "selected" : "hidden"}`}
       id={`navmenu-item-${nameLowerDashed}`}
       tabIndex={0}
+      onClick={() => onClick(nameLowerDashed)}
     >
       <div className="button-text">{name}</div>
     </div>
