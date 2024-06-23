40f223bf8a9a0d3e176cee9b2b7a4a15da1707ff
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-23 16:29:20 +0200

    navbutton sections now work

--- app/components/NavButtonSection.tsx
+++ app/components/NavButtonSection.tsx
@@ -1,12 +1,24 @@
 import Card from "./Card";
 
 function NavButtonSection({ name }: { name: string }) {
+  const nameLowerDashed = name.toLowerCase().replace(" ", "-");
+
   return (
     <Card
       className="navbutton card-region-child"
-      id={`navbutton-${name.toLowerCase()}`}
+      id={`navbutton-${nameLowerDashed}`}
       tabIndex={0}
       individualEffect={false}
+      onClick={() => {
+        const section = document.getElementById(nameLowerDashed);
+        if (!section) {
+          return;
+        }
+        window.scrollTo({
+          top: section.offsetTop - 100,
+          behavior: "smooth",
+        });
+      }}
     >
       <div className="button-text">{name}</div>
     </Card>
