eef3cdef39a22a47cc19f8d8ed06e56a523df02e
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-27 23:40:43 +0200

    footer current year

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,11 @@
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <div className="footer-text">
-        Copyright &copy; 2024 Marcus Alenius. All rights reserved.
+        Copyright &copy; {currentYear} Marcus Alenius. All rights reserved.
       </div>
     </footer>
   );
