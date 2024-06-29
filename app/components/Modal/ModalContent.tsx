77bd0e767ddae7397c38653a00313cdfe25b6675
Marcus Alenius <marcus.alenius@gmail.com>
2024-06-29 23:24:13 +0200

    added cards in modal

--- app/components/Modal/ModalContent.tsx
+++ app/components/Modal/ModalContent.tsx
@@ -1,3 +1,8 @@
+import "./ModalContentCard.css";
+
+import CardRegion from "../Card/CardRegion";
+import Card from "../Card/Card";
+
 type Props = {
   categoryData: { [key: string]: any };
 };
@@ -6,6 +11,19 @@ function ModalContent({ categoryData }: Props) {
   return (
     <div className="modal-content">
       <h2>{categoryData.title} Projects</h2>
+      <CardRegion className="card-layout">
+        {categoryData.projects.map((projectData: { [key: string]: any }) => {
+          return (
+            <Card
+              className="modal-content-card card-region-child"
+              individualEffect={false}
+            >
+              <h3>{projectData.title}</h3>
+              <p className="paragraph-small">{projectData.description}</p>
+            </Card>
+          );
+        })}
+      </CardRegion>
     </div>
   );
 }
