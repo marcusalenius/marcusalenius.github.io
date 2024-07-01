a057393999fadf5358e6246e24eec44aec77e544
Marcus Alenius <marcus.alenius@gmail.com>
2024-07-01 16:16:29 +0200

    removed dead code from herocard

--- app/components/Hero/HeroCard.tsx
+++ app/components/Hero/HeroCard.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import Card from "../Card/Card";
 
 function HeroCard() {
@@ -11,48 +9,6 @@ function HeroCard() {
         alt="Marcus Alenius"
         draggable={false}
       />
-      {/* <img
-        src="images/hero-image-desktop.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-desktop"
-      />
-      <img
-        src="images/hero-image-tablet.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-tablet"
-      />
-      <img
-        src="images/hero-image-medium-skinny.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-medium-skinny"
-      />
-      <img
-        src="images/hero-image-skinny.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-skinny"
-      />
-      <img
-        src="images/hero-image-mobile.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-mobile"
-      />
-      <img
-        src="images/hero-image-mobile-small.png"
-        alt="large image of Marcus Alenius"
-        draggable="false"
-        className="hero-image"
-        id="hero-image-mobile-small"
-      /> */}
     </Card>
   );
 }
