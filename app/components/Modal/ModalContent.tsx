1bc085c57bfff1293a5b788d103e5b130667d700
Marcus Alenius <marcus.alenius@gmail.com>
2024-07-01 11:50:13 +0200

    white overlay

--- app/components/Modal/ModalContent.tsx
+++ app/components/Modal/ModalContent.tsx
@@ -9,11 +9,14 @@ function ModalContent({ categoryData }: Props) {
   return (
     <div className="modal-content">
       <h2>{categoryData.title} Projects</h2>
-      <CardRegion className="card-layout">
-        {categoryData.projects.map((projectData: { [key: string]: any }) => {
-          return <ModalContentCard projectData={projectData} />;
-        })}
-      </CardRegion>
+      <div className="modal-cards-container">
+        <CardRegion className="card-layout">
+          {categoryData.projects.map((projectData: { [key: string]: any }) => {
+            return <ModalContentCard projectData={projectData} />;
+          })}
+        </CardRegion>
+        <div className="modal-cards-overlay"></div>
+      </div>
     </div>
   );
 }
