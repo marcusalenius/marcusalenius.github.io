a8fedbfa8e06099bf796ab0445c401f660518491
Marcus Alenius <marcus.alenius@gmail.com>
2024-07-19 23:04:53 +0200

    hero button modal

--- app/components/Hero/HeroButton.tsx
+++ app/components/Hero/HeroButton.tsx
@@ -1,24 +1,42 @@
+"use client";
+
 import "./HeroButton.css";
+
+import { useState } from "react";
 
 import Card from "../Card/Card";
 import PlusCrossButton from "../PlusCrossButton/PlusCrossButton";
+import Modal from "../Modal/Modal";
 
 type Props = {
   buttonData: { [key: string]: any };
 };
 
 function HeroButton({ buttonData }: Props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <Card
-      className="card hero-button card-region-child"
-      tabIndex={0}
-      individualEffect={false}
-    >
-      <div className="hero-button-content">
-        <div className="button-text">{buttonData.text}</div>
-        <PlusCrossButton />
-      </div>
-    </Card>
+    <>
+      <Card
+        className="card hero-button card-region-child"
+        tabIndex={0}
+        individualEffect={false}
+        onClick={() => setIsModalOpen(!isModalOpen)}
+      >
+        <div className="hero-button-content">
+          <div className="button-text">{buttonData.text}</div>
+          <PlusCrossButton />
+        </div>
+      </Card>
+      <Modal
+        categoryData={{
+          title: "AI/ML",
+          projects: [],
+        }}
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
+    </>
   );
 }
 
