f1c139b735b9748e10799c6161d1f5551c513f01
Marcus Alenius <marcus.alenius@gmail.com>
2024-07-19 22:23:55 +0200

    fixed hero button dark mode

--- app/components/Hero/HeroButton.tsx
+++ app/components/Hero/HeroButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 function HeroButton({ buttonData }: Props) {
   return (
-    <Card className="hero-button card-region-child" tabIndex={0}>
+    <Card className="card hero-button card-region-child" tabIndex={0}>
       <div className="hero-button-content">
         <div className="button-text">{buttonData.text}</div>
         <PlusCrossButton />
