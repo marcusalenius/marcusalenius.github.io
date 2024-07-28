7ceeb5b20f72d6aa161e65a2e5cda0bb99f4d92f
Marcus Alenius <marcus.alenius@gmail.com>
2024-07-28 16:32:04 +0200

    added meta and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import "./typography.css";
 
+import type { Metadata } from "next";
+
 import data from "../data.json";
 
 import Nav from "./components/Nav/Nav";
 import HomeContainer from "./components/HomePage/HomeContainer";
 import Footer from "./components/Footer/Footer";
+
+export const metadata: Metadata = {};
 
 function Home() {
   return (
