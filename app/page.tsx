74d983f16478ee3d17561d2f9228aee0710ea042
Marcus Alenius <marcus.alenius@gmail.com>
2024-08-02 12:02:18 +0200

    disabled ssr for nav on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import "./typography.css";
 
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 import data from "../data.json";
 
-import Nav from "./components/Nav/Nav";
+// import Nav from "./components/Nav/Nav";
+const DynamicNav = dynamic(() => import("./components/Nav/Nav"), {
+  ssr: false,
+});
 import HomeContainer from "./components/HomePage/HomeContainer";
 import Footer from "./components/Footer/Footer";
 
@@ -13,7 +17,7 @@ export const metadata: Metadata = {};
 function HomePage() {
   return (
     <>
-      <Nav data={data} homePage={true} />
+      <DynamicNav data={data} homePage={true} />
       <HomeContainer data={data} />
       <Footer />
     </>
