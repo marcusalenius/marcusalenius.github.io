b75328348dc8eee22236880bf95ee193a11a485b
Marcus Alenius <marcus.alenius@gmail.com>
2025-07-02 21:26:27 -0700

    original modifications

--- app/components/ExperienceCard/ExperienceCard.tsx
+++ app/components/ExperienceCard/ExperienceCard.tsx
@@ -9,11 +9,18 @@ type Props = {
 export default function ExperienceCard({ cardData }: Props) {
   return (
     <Card className="experience-card card card-region-child">
-      <h3>{cardData.title}</h3>
-      <p className="paragraph-small">
-        {cardData.organization}, {cardData.start} &ndash; {cardData.end},{" "}
-        {cardData.location}
+      <h4>{cardData.title}</h4>
+      <p className="paragraph-small experience-organization">
+        {cardData.organization}
       </p>
+      <h5>
+        {cardData.location}, {cardData.start} &ndash; {cardData.end}
+      </h5>
+      {cardData.description && (
+        <p className="paragraph-small experience-description">
+          {cardData.description}
+        </p>
+      )}
     </Card>
   );
 }
