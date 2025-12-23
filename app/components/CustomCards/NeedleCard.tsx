947ecb4e28119ccf78c8586dcf87ad1a8d06931c
Marcus Alenius <marcus.alenius@gmail.com>
2025-12-23 11:08:12 +0100

    speech transformer card

--- app/components/CustomCards/NeedleCard.tsx
+++ app/components/CustomCards/NeedleCard.tsx
@@ -2,27 +2,15 @@
 
 import "./NeedleCard.css";
 
-import dynamic from "next/dynamic";
-import Image from "next/image";
-
 import Card from "../Card/Card";
 import InternalLink from "../Links/InternalLink";
 import NeedleCardSvg from "./NeedleCardSvg";
-
-const Video = dynamic(() => import("../Media/Video"), {
-  ssr: false,
-});
 
 type Props = {
   cardData: { [key: string]: any };
 };
 
 export default function NeedleCard({ cardData }: Props) {
-  function formatSubtitle(subtitle?: string): string {
-    if (!subtitle) return "";
-    const normalized = subtitle.toLowerCase().replace(/\bai\b/g, "AI");
-    return normalized.charAt(0).toUpperCase() + normalized.slice(1);
-  }
   return (
     <Card
       className="card card-region-child"
--- app/components/CustomCards/SpeechTransformerCard.tsx
+++ app/components/CustomCards/SpeechTransformerCard.tsx
@@ -2,42 +2,30 @@
 
 import "./SpeechTransformerCard.css";
 
-import dynamic from "next/dynamic";
-
 import Card from "../Card/Card";
 import InternalLink from "../Links/InternalLink";
-
-const Video = dynamic(() => import("../Media/Video"), {
-  ssr: false,
-});
+import SpeechTransformerSvg from "./SpeechTransformerSvg";
 
 type Props = {
   cardData: { [key: string]: any };
 };
 
-export default function SpeechTransformerCard({ cardData }: Props) {
-  const speechTransformerPostLink = "/speech-transformer";
-  function formatSubtitle(subtitle?: string): string {
-    if (!subtitle) return "";
-    const normalized = subtitle.toLowerCase().replace(/\bai\b/g, "AI");
-    return normalized.charAt(0).toUpperCase() + normalized.slice(1);
-  }
+export default function NeedleCard({ cardData }: Props) {
   return (
     <Card
       className="card card-region-child"
       id="speech-transformer-card"
       individualEffect={false}
-      href={speechTransformerPostLink}
+      href={cardData.link}
     >
       <div className="card-images">
-        <div className="frosted-pane">
-          <h3>{cardData.title}</h3>
-          <p className="paragraph-small">{formatSubtitle(cardData.subtitle)}</p>
-          {/* href empty so that InternalLink gets rendered as a div */}
-          <InternalLink href="">
-            <p className="card-link">Learn more</p>
-          </InternalLink>
-        </div>
+        <h3>{cardData.title}</h3>
+        <p className="paragraph-small">{cardData.subtitle}</p>
+        {/* href empty so that InternalLink gets rendered as a div */}
+        <InternalLink href="">
+          <p className="card-link">Learn more</p>
+        </InternalLink>
+        <SpeechTransformerSvg />
       </div>
     </Card>
   );
