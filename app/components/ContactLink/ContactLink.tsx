e49e791c898a91a59dc5c40a18b3966fbcda1681
Marcus Alenius <marcus.alenius@gmail.com>
2025-12-27 12:34:53 +0100

    changed contanct links header to h5

--- app/components/ContactLink/ContactLink.tsx
+++ app/components/ContactLink/ContactLink.tsx
@@ -7,7 +7,8 @@ type Props = {
 export default function ContactLink({ contactData }: Props) {
   return (
     <div className="contact-link" key={contactData.id}>
-      <p className="small-header">{contactData.title.toUpperCase()}</p>
+      {/* <p className="small-header">{contactData.title.toUpperCase()}</p> */}
+      <h5>{contactData.title.toUpperCase()}</h5>
       <a href={contactData.link} target="_blank">
         <p className="paragraph-large">{contactData.text}</p>
       </a>
